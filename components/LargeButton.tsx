8f96194727879792ad1e4e05b2a5821a4f2c59f5
SimonBucko <bucino.36@gmail.com>
2022-05-18 20:21:11 +0200

    passed function

--- components/LargeButton.tsx
+++ components/LargeButton.tsx
@@ -3,14 +3,15 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Palette from '../styles/pallete';
 import globalStyles from "../styles/global"
 
-interface LargeButton {
-    text:string
+interface ILargeButton {
+    text:string,
+    handleOnClick:Function
 }
 
 
-const LargeButton = ({ text }:LargeButton) => {
+const LargeButton = ({ text,handleOnClick }:ILargeButton) => {
     return (
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={() => handleOnClick()}>
             <Text style={{...globalStyles.text,...styles.textStyle}}>{text}</Text>
         </TouchableOpacity>
     );
--- screens/onboarding/EventsScreen.tsx
+++ screens/onboarding/EventsScreen.tsx
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import globalStyles from "../../styles/global"
 import { LargeButton } from '../../components';
 
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParamList } from "../../typings/navigations";
+import { useNavigation } from '@react-navigation/native';
+
+type ScreenNavigationType = NativeStackNavigationProp<
+    StackParamList,
+    "InitialScreen"
+>
+
 export default function EventsScreen() {
+    const navigation = useNavigation<ScreenNavigationType>()
+
+    const handleClick = () => {
+        console.log("hey")
+        navigation.navigate("ChatScreen")
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>Events Screen</Text>
-            <LargeButton text="Next"/>
+            <LargeButton text="Next" handleOnClick={handleClick}/>
         </View>
     );
 }
