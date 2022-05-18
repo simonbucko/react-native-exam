255a12e27960653487055cc72c5520e92cd56815
SimonBucko <bucino.36@gmail.com>
2022-05-18 21:22:10 +0200

    finshed onboarding screens

--- screens/onboarding/HistoryScreen.tsx
+++ screens/onboarding/HistoryScreen.tsx
@@ -1,15 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import globalStyles from "../../styles/global"
+import { LargeButton } from '../../components';
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParamList } from "../../typings/navigations";
+import { useNavigation } from '@react-navigation/native';
+import Palette from '../../styles/pallete';
+
+type ScreenNavigationType = NativeStackNavigationProp<
+    StackParamList,
+    "HistoryScreen"
+>
 
 export default function HistoryScreen() {
+    const navigation = useNavigation<ScreenNavigationType>()
+
+    const handleClick = () => {
+        navigation.navigate("Signup")
+    }
+
     return (
         <View style={globalStyles.container}>
-            <Text>History Screen</Text>
+            <Image progressiveRenderingEnabled={true} style={styles.image} source={require("../../assets/pics/history.png")}/>
+            <Text style={styles.text}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</Text>
+            <View style={styles.dotsContainer}>
+                <View style={{...styles.dot}}></View>
+                <View style={{...styles.dot}}></View>
+                <View style={{...styles.dot,...styles.activeDot}}></View>
+            </View>
+            <LargeButton text="Next" handleOnClick={handleClick}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    image:{
+        marginBottom: 40,
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 16,
+        color: Palette.gray,
+        marginBottom: 24
+    },
+    dotsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: 24
 
+    },
+    dot:{
+        height:8,
+        width:8,
+        backgroundColor: Palette.light_gray,
+        borderRadius: 50,
+        marginRight: 8
+    },
+    activeDot: {
+        backgroundColor: Palette.primary
+    }
 })  
