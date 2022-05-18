eb0842d1e87cef5018709711c4301718b6566074
SimonBucko <bucino.36@gmail.com>
2022-05-18 07:35:07 +0200

    started my own work

---
+++ styles/global.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flex: 1,
+    },
+    text: {
+        fontFamily: "roboto-regular",
+        fontSize: 18,
+        color: "#333"
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20
+    }
+})
---
+++ styles/pallete.ts
@@ -0,0 +1,7 @@
+enum Palette {
+    primary = "#32305D",
+    bright = "#fff",
+    dark = "#000",
+    gray = "#707070",
+    secondary = "#FFBA00",
+}
