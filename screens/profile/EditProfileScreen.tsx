5d080c0b6515aca5c6b6585015f674c7dcfbbf5e
SimonBucko <bucino.36@gmail.com>
2022-05-24 22:26:51 +0200

    s

--- screens/profile/EditProfileScreen.tsx
+++ screens/profile/EditProfileScreen.tsx
@@ -7,6 +7,8 @@ import { RootState } from '../../store';
 import { updateUserProfile } from '../../store/actions/user.actions';
 import globalStyles from '../../styles/global';
 import Palette from '../../styles/pallete';
+import { Snackbar } from 'react-native-paper';
+import {Keyboard} from 'react-native'
 
 
 export default function EditProfileScreen() {
@@ -15,9 +17,14 @@ export default function EditProfileScreen() {
     const [name, setName] = useState(displayName)
     const [studyProgramme, setStudyProgramme] = useState(studyProgram)
     const dispatch = useDispatch();
+    const [visible, setVisible] = useState(false);
 
+    const onToggleSnackBar = () => setVisible(!visible);
+    const onDismissSnackBar = () => setVisible(false);
     const handleSave = () => {
+        Keyboard.dismiss()
         dispatch(updateUserProfile(name,studyProgramme))
+        onToggleSnackBar()
     }
 
     return (
@@ -25,6 +32,17 @@ export default function EditProfileScreen() {
             <InputWithLabel label='WHAT IS YOUR NAME?' value={name} handleValueChange={setName} placeholder={"John Wick"} style={styles.input}/>
             <InputWithLabel label='STUDY PROGRAMME' value={studyProgramme} handleValueChange={setStudyProgramme} placeholder={"Business"} style={styles.input}/>
             <LargeButton text='Save Changes' disabled={name === "" || studyProgramme === ""} handleOnClick={handleSave} style={styles.button}/>
+            <Snackbar
+                visible={visible}
+                onDismiss={onDismissSnackBar}
+                action={{
+                    label: 'Ok',
+                    labelStyle: {color: Palette.bright}
+                }}
+                style={styles.snackBar}
+                >
+                Your profile has been saved.
+            </Snackbar>
         </View>
     );
 }
@@ -37,5 +55,8 @@ const styles = StyleSheet.create({
     },
     button:{
         marginTop: 24 
+    },
+    snackBar: {
+        backgroundColor: Palette.success
     }
 })
